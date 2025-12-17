f61cb5121de416c2327699385ba9f573ec3799de
Jair Rueda <123614716+JairAlexey@users.noreply.github.com>
2025-12-16 19:33:53 -0500

    refactor: remove analysis_data field and update participantlog model

--- backend/events/migrations/0011_remove_participantlog_file_participantlog_url.py
+++ backend/events/migrations/0011_remove_participantlog_file_participantlog_url.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0009_participantlog_timestamp'),
+        ('events', '0010_participantevent_is_blocked'),
     ]
 
     operations = [
